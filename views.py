e46479aae0cc951c8be97f7e791bc9e55a8b28bb
corinnelhh <corinne.hutchinson@gmail.com>
2014-08-29 20:32:34 -0700

    added function to get submissions and redirect to home page with reply

--- views.py
+++ views.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask import render_template
+from flask import render_template, request, url_for, redirect
 
 from detective import FortuneTeller
 
@@ -10,8 +10,16 @@ app = Flask(__name__)
 @app.route('/')
 def show_main_page():
     u"""Displays base.html."""
-    return render_template('base.html')
+    prediction = request.args.get('prediction')
+    return render_template('base.html', prediction=prediction)
 
+
+@app.route('/submit', methods=['GET', 'POST'])
+def get_input_text():
+    u"""Receive input text from site."""
+    submission = request.form['submission']
+    answer = "Hey, you said {}".format(submission)
+    return redirect(url_for('show_main_page', prediction=answer))
 
 if __name__ == '__main__':
     ft = FortuneTeller()
