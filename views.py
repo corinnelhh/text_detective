60e9ac5a3464aec957d55dd34ba252cb2e37793f
corinnelhh <corinne.hutchinson@gmail.com>
2014-09-02 20:24:06 -0700

    changed views.py to debug=false for deployment

--- views.py
+++ views.py
@@ -28,7 +28,8 @@ def get_input_text():
 
 if __name__ == '__main__':
     dt = Detective_()
-    app.run(debug=True)
-    # from wsgiref.simple_server import make_server
-    # srv = make_server('localhost', 8000, app)
-    # srv.serve_forever()
+    # app.run(debug=True)
+    app.run(debug=False)
+    from wsgiref.simple_server import make_server
+    srv = make_server('localhost', 8000, app)
+    srv.serve_forever()
